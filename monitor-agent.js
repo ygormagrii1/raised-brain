fd7912ec7c7e4a67226012fd690e34490fbec3bf
Ygor <ygor@agenciaraised.com.br>
2026-04-17 06:27:06 -0300

    fix: correct .env.local path for remote execution

--- monitor-agent.js
+++ monitor-agent.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('dotenv').config({ path: '../../.env.local' });
+require('dotenv').config({ path: './.env.local' });
 const fs = require('fs');
 const path = require('path');
 const twilio = require('twilio');
